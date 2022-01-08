4820604ddcfa9116f6b627d97704657fda728f67
Noah <nosherway@yahoo.com>
2022-01-08 09:08:53 -0600

    final modal complete

--- Develop/script.js
+++ Develop/script.js
@@ -9,11 +9,14 @@ const generateBtn = document.querySelector("#generate");
 const inputBtn = document.querySelector("#criteria");
 const cancelBtn1 = document.querySelector("#cancel1")
 const cancelBtn2 = document.querySelector("#cancel2")
+const cancelBtn3 = document.querySelector("#cancel3")
 const nextBtn1 = document.querySelector("#next1")
+const nextBtn2 = document.querySelector("#next2")
 const inputModal = document.getElementById("myModal")
 const inputModal2 = document.getElementById("myModal2")
-const passwordModalTxt = document.getElementById("passwordtxt")
+const inputModal3 = document.getElementById("myModal3")
 const passwordModalTxt2 = document.getElementById("passwordtxt2")
+const passwordModalTxt3 = document.getElementById("passwordtxt3")
 const span = document.getElementsByClassName("close")[0];
 
 // Input modal 
@@ -25,7 +28,6 @@ inputBtn.onclick = function() {
 cancelBtn1.onclick = function() {
     inputModal.style.display = "none";
     inputModal2.style.display = "none";
-    passwordModalTxt.value ='';
     
 }
 cancelBtn2.onclick = function() {
@@ -33,6 +35,11 @@ cancelBtn2.onclick = function() {
   inputModal2.style.display = "none";
 
   passwordModalTxt2.value ='';
+}
+cancelBtn3.onclick = function () {
+  inputModal3.style.display = "none";
+
+  passwordModalTxt3.value ='';
 }
 
 span.onclick = function() {
@@ -42,6 +49,11 @@ nextBtn1.onclick = function() {
   inputModal.style.display = "none";
   inputModal2.style.display = "block";
 }
+
+nextBtn2.onclick = function() {
+  inputModal2.style.display = "none";
+  inputModal3.style.display = "block";
+} 
 
 window.onclick = function(event) {
     if (event.target == inputModal) {
--- README.md
+++ README.md
@@ -1,19 +1,31 @@
 # Password Generator Starter Code
 
 GIVEN I need a new, secure password
+
+<!-- Step 1: Build / Style modal for each step -->
+
 WHEN I click the button to generate a password
 THEN I am presented with a series of prompts for password criteria
 
-
+<!-- Step 2: Update Modal 2 that states password criteria guidlines   -->
 WHEN prompted for password criteria
 THEN I select which criteria to include in the password
+
+<!-- Step 3 Set up modal to allow text input of a number between 8-128 -->
+
 WHEN prompted for the length of the password
 THEN I choose a length of at least 8 characters and no more than 128 characters
+
+<!-- Step 4  -->
+
 WHEN prompted for character types to include in the password
 THEN I choose lowercase, uppercase, numeric, and/or special characters
+<!-- Step 5  -->
 WHEN I answer each prompt
 THEN my input should be validated and at least one character type should be selected
 WHEN all prompts are answered
+<!-- Step 6 -->
 THEN a password is generated that matches the selected criteria
 WHEN the password is generated
+<!-- Step 7 -->
 THEN the password is either displayed in an alert or written to the page
