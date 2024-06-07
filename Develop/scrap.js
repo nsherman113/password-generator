ca687698d584ca7a6cdebe68dff68976c8cef162
Noah <nosherway@yahoo.com>
2024-06-06 19:24:39 -0500

    update

---
+++ Develop/scrap.js
@@ -0,0 +1,8 @@
+console.log('test');
+
+function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min; // max & min both included 
+  }
+  console.log(getRandomIntInclusive(2,650))
